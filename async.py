ae228fa624e1778d44aa416a099f261669fe64fe
MineevHome <i@mineevburyat.ru>
2022-10-29 18:22:33 +0800

    асинхронные запросы к словарю бурлэнг

--- async.py
+++ async.py
@@ -1,32 +1,53 @@
 import asyncio
 import time
 from aiohttp import ClientSession
+import json
 
 
-async def get_burtranslate(word):
+async def get_burtranslate(word:str):
     async with ClientSession() as session:
         url = f'http://burlang.ru/api/v1/russian-word/translate'
+        word = word.strip()
         params = {'q': word}
 
         async with session.get(url=url, params=params) as response:
-            weather_json = await response.json()
-            return {word: weather_json}
+            translate = []
+            if response.status == 200:
+                translate_json = await response.json()
+                for item in translate_json.get('translations'):
+                    translate.append(item.get('value'))
+            return {word: translate}
 
 
-async def main(words_list):
+async def main_corutine(words_list):
     tasks = []
     for word in words_list:
         tasks.append(asyncio.create_task(get_burtranslate(word)))
-    print(tasks)
     results = await asyncio.gather(*tasks)
     for result in results:
         print(result)
+        try:
+            data = json.load(open('ruburdic_asinc.json', encoding='utf-8'))
+        except:
+            data = {}
+        print('save')
+        json.dump({**data, **result}, 
+            open('ruburdic_asinc.json', mode='w', encoding='utf-8'), indent=2, ensure_ascii=False)
 
-words_list = ['собака', 'бабушка', 'дедушка', 'брат', 'мама',
-          'папа', 'русский', 'китаец', 'бурят', 'бурятия', 'красный']
+if __name__ == "__main__":
+    dic = {}
+    with open('russian.utf-8.txt', 'r') as f:
+        while True:
+            words_list = f.readlines(10240)
+            print(words_list)
+            if words_list:
+                asyncio.run(main_corutine(words_list))
+        f.close()
 
-print(time.strftime('%X'))
+# words_list = ['rfnfhcb', 'зеленый']
 
-asyncio.run(main(words_list))
+# print(time.strftime('%X'))
 
-print(time.strftime('%X'))
+
+
+# print(time.strftime('%X'))
